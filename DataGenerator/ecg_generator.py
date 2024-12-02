7e235b91f1c570e7eb73d9e589535d42ac3cd9db
Mitza23 <mitzagrebla@yahoo.com>
2024-12-02 21:47:40 +0200

    Generate ecg data

--- DataGenerator/ecg_generator.py
+++ DataGenerator/ecg_generator.py
@@ -1,11 +1,39 @@
-import neurokit2 as nk  # Load the package
+import neurokit2 as nk
+import numpy as np
 
-# simulated_ecg = nk.ecg_simulate(duration=8, sampling_rate=200, heart_rate=80)
-#
-# nk.signal_plot(simulated_ecg, sampling_rate=200)  # Visualize the signal
-#
-# processed_ecg = nk.ecg_process(simulated_ecg, sampling_rate=200)  # Process the signal
 
-simulated_ecg = nk.ecg_simulate(duration=8, sampling_rate=200, method="daubechies")
+def compute_segment(signals, start_metric, end_metric):
+    # Extract the indices where start_metric and end_metric are not zero
+    start_indices = np.where(signals[start_metric] != 0)[0]
+    end_indices = np.where(signals[end_metric] != 0)[0]
 
-nk.signal_plot(simulated_ecg, sampling_rate=200)
+    # Truncate the longer array to match the length of the shorter one
+    min_length = min(len(start_indices), len(end_indices))
+    start_indices = start_indices[:min_length]
+    end_indices = end_indices[:min_length]
+
+    return end_indices - start_indices
+
+def generate_segments(duration=3600):
+    sampling_rate = 1000
+
+    ecg = nk.ecg_simulate(duration=duration, sampling_rate=sampling_rate)
+    nk.signal_plot(ecg, sampling_rate=sampling_rate)
+
+    signals, _ = nk.ecg_process(ecg, sampling_rate=sampling_rate)
+
+    # COMPUTE PR SEGMENT
+    PR_segment = compute_segment(signals, 'ECG_P_Onsets', 'ECG_R_Onsets')
+
+    # COMPUTE QRS SEGMENT
+    QRS_segment = compute_segment(signals, 'ECG_R_Onsets', 'ECG_R_Offsets')
+
+    # COMPUTE ST SEGMENT
+    ST_segment = compute_segment(signals, 'ECG_R_Offsets', 'ECG_T_Onsets')
+
+    return {
+            "PR_segment": PR_segment,
+            "QRS_segment": QRS_segment,
+            "ST_segment": ST_segment
+        }
+
